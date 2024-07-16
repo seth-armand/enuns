946fc02791ba14d7aff8ad12a80ce5915dd96d7c
enzo <mestacherenotu@gmail.com>
2024-07-16 20:23:52 -0300

    inicio exercicio 1

---
+++ src/main/java/coisascurso/enuns/EnunsApplication.java
@@ -0,0 +1,13 @@
+package coisascurso.enuns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnunsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnunsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/coisascurso/enuns/EnunsApplicationTests.java
@@ -0,0 +1,13 @@
+package coisascurso.enuns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnunsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
